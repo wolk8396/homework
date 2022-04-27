9b8848897253e9c27db63615dc45dc5fd766adff
wolk8396 <88962946+wolk8396@users.noreply.github.com>
2022-04-27 20:04:57 +0300

    home

---
+++ Homework/homework_27_04/index.js
@@ -0,0 +1,62 @@
+const date = new Date;
+const year = date.getUTCFullYear();
+const years = new Map ([
+  [1,'first'],
+  [2,'second'],
+  [3,'third'],
+  [4,'fourth'],
+  [5,'fifth'],
+]);
+
+class Users {
+
+  constructor(name, surname ,sex) {
+    this.name = name;
+    this.surname = surname;
+    this.sex = sex;
+  }
+
+  getPronouns() {
+
+   if (this.sex === 'female' ) {
+      this.sex = 'her';
+   } else if (this.sex === 'male') {
+      this.sex = 'his';
+   }
+  }
+};
+
+class Students extends Users {
+
+  constructor(name, surname, sex, admission) {
+    super(name, surname, sex);
+    this.admission = admission;
+    this.fullName = `${this.name} ${this.surname}`;
+
+    console.log(`${this.fullName} entered the university in ${this.admission}`);
+  }
+
+  getCourse(year) {
+    let current = 0;
+    current = year - this.admission;
+
+    (current < 6) ?
+      console.log(`${this.fullName} is in ${this.sex} ${years.get(current)} course`) :
+      console.log(`${this.fullName} graduated university ${current} years ago`);
+  }
+};
+
+const kate = new Students('Kate', 'Helton', 'female', 2018);
+const mattew = new Students('Matthew', 'Perry', 'male',2019);
+const rachel = new Students('Rachel', 'Green', 'female',2015);
+
+kate.getPronouns();
+kate.getCourse(year);
+
+mattew.getPronouns();
+mattew.getCourse(year);
+
+
+rachel.getPronouns();
+rachel.getCourse(year);
+
